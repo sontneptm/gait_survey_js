76c114502ec12ba8603e916825943802d03c3f60
Seungmin Jeong <sontneptm2@gmail.com>
2022-04-07 17:36:04 +0900

    update

--- src/components/Survey.js
+++ src/components/Survey.js
@@ -1,5 +1,20 @@
+import styles from "./Survey.module.css";
+
 function Survey() {
-  return <h1>survey!</h1>;
+  return (
+    <section title=".squaredThree">
+      <div className={styles.squaredThree}>
+        <input
+          type="checkbox"
+          value="None"
+          id="squaredThree"
+          name="check"
+          checked
+        />
+        <label for="squaredThree"></label>
+      </div>
+    </section>
+  );
 }
 
 export default Survey;
