25b1a4b505c29d80036da0395ac59c4cefa09ca5
Seungmin Jeong <sontneptm2@gmail.com>
2022-04-07 17:00:25 +0900

    update

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -2,37 +2,38 @@ import styles from "./Login.module.css";
 
 function Login() {
   return (
-    <div className={styles.wrapper}>
-      <form className={styles.form_signin}>
-        <h2 className={styles.form_signin_heading}>Please login</h2>
-        <input
-          type={styles.text}
-          className={styles.form_control}
-          name="username"
-          placeholder="성함"
-          required=""
-          autoFocus=""
-        />
-        <input
-          type="password"
-          className={styles.form_control}
-          name="password"
-          placeholder="소속"
-          required=""
-        />
-        <label className={styles.checkbox}>
-          <input
-            type="checkbox"
-            value="remember-me"
-            id="rememberMe"
-            name="rememberMe"
-          />{" "}
-          Remember me
-        </label>
-        <button className="btn btn-lg btn-primary btn-block" type="submit">
-          Login
-        </button>
-      </form>
+    <div>
+      <div className={styles.logo__box}>
+        <img
+          className={styles.logo}
+          src="images/msdl_logo.png"
+          alt="please report sontneptm2@gmail"
+        ></img>
+      </div>
+      <div className={styles.login_page}>
+        <div className={styles.form}>
+          <h1>보행실험 설문조사</h1>
+          <h4>답변하시는분의 정보를 입력해주세요</h4>
+          <form className={styles.register_form}>
+            <input type="text" placeholder="test" />
+            <input type="password" placeholder="소속" />
+            <input type="text" placeholder="email address" />
+            <button>create</button>
+            <p className="message">
+              Already registered? <a href="#">Sign In</a>
+            </p>
+          </form>
+          <form className={styles.login_form}>
+            <input type="text" placeholder="이름" required />
+            <input type="text" placeholder="직위" required />
+            <input type="text" placeholder="소속" required />
+            <button>설문시작</button>
+            <p className={styles.message}>
+              연구 내용이 궁금하신가요? <a href="#">읽어보기</a>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
